d845b9e384a38c0d479c2b457375734817f75221
MiniSoda17 <isaac.arli@gmail.com>
2024-09-23 23:12:27 +1000

    Creates react project within frontend package

---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,9 @@
+import styles from './NavigationBar.module.css';
+
+const NavigationBar = () => {
+  return (<div>
+    Hello World
+  </div>)
+}
+
+export default NavigationBar;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import styles from './HomeScreen.module.css';
+
+const HomeScreen = () => {
+  return <div>Hello World</div>;
+};
+
+export default HomeScreen;
